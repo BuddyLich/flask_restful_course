85e090ebc7af020fd5d896661c6ca127bf061e46
Zhen <zhengzhen048@gmail.com>
2021-02-21 16:05:39 +1100

    chapter 142 - completed everything up to chapter 142

---
+++ section_11/code/app.py
@@ -0,0 +1,40 @@
+import os
+
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister, User
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+from db import db
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', "sqlite:///data.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
+# means flask extensions like flask_jwt can raise its own errors
+app.secret_key = "someSecretKeyThatIsNotMeantToDisplayedHere"
+api = Api(app)
+db.init_app(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)
+# it create a new route /auth
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ section_11/code/resources/item.py
@@ -0,0 +1,72 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        "price",
+        type=float,
+        required=True,
+        help="This field cannot be blank"
+    )
+
+    parser.add_argument(
+        "store_id",
+        type=int,
+        required=True,
+        help="Every item needs a store id"
+    )
+
+    # for the purpose of practice, only the get route require authentication
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {"message": "Item not found"}, 404
+
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {"message": f"An item with name {name} already exists"}, 400
+
+        data = Item.parser.parse_args()
+        item = ItemModel(name, **data)
+
+        try:
+            item.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the item"}, 500
+
+        return item.json(), 201
+
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+
+        return {'message': 'Item Deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+        # updated_item = ItemModel(name, data["price"])
+        if item is None:
+            item = ItemModel(name, **data)
+        else:
+            item.price = data['price']
+            item.store_id = data['store_id']
+
+        item.save_to_db()
+        return item.json()
+
+
+class ItemList(Resource):
+    def get(self):
+        items = ItemModel.find_all()
+        return {"items": [item.json() for item in items]}
+
+        # or you can do this:
+        # return {"items": list(map(lambda x: x.json, items))}
